d5193655691bf2a8e16f6fa4b3af3ecdd8c71e25
Andrei Sharafullin <andrei.sharafullin@tomtom.com>
2020-09-11 10:10:16 +0200

    renamed package

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='ha-bt-extender',
+setup(name='ha_bt_extender',
       version='0.1',
       description='The home assistant bluetooth extender',
       url='https://github.com/sharafullin/ha-bt-extender',
       author='Andrei Sharafullin',
       author_email='sharafullin@gmail.com',
       license='MIT',
-      packages=['ha-bt-extender'],
+      packages=['ha_bt_extender'],
       zip_safe=False)
