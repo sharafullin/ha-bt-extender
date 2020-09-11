e7cc1f4875b33b5fcce8ca5099b405f9a67de669
Andrei Sharafullin <andrei.sharafullin@tomtom.com>
2020-09-11 09:32:18 +0200

    initial implemetation of package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ha-bt-extender',
+      version='0.1',
+      description='The home assistant bluetooth extender',
+      url='https://github.com/sharafullin/ha-bt-extender',
+      author='Andrei Sharafullin',
+      author_email='sharafullin@gmail.com',
+      license='MIT',
+      packages=['ha-bt-extender'],
+      zip_safe=False)
