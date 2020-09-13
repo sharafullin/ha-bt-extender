d4a274c98a4704438cbb11ebafa5aa28d2ae4e48
Andrei Sharafullin <andrei.sharafullin@tomtom.com>
2020-09-13 13:12:47 +0200

    autorefresh

--- core/service.py
+++ core/service.py
@@ -1,11 +1,18 @@
 import time
 import git
 from git import Repo
+import os
 
 repo = Repo("/opt/ha-bt-extender")
+origin = repo.remotes.origin
+origin.pull()
+
 remoteRepo = Repo("/opt/ha-bt-extender-remote")
 
 while true:
-    origin = remoteRepo.remotes.origin
-    origin.pull()
+    remoteOrigin = remoteRepo.remotes.origin
+    remoteOrigin.pull()
+    if repo.active_branch.commit.hexsha != remoteRepo.active_branch.commit.hexsha:
+        os.system('reboot')
+
     time.sleep(60)
