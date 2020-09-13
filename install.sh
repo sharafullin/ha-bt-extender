cc13988cfeaacaa0dc42d9b9bf670ab13b129262
Andrei Sharafullin <andrei.sharafullin@tomtom.com>
2020-09-13 11:51:11 +0200

    install core service

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-sudo apt-get update && sudo apt-get --assume-yes install python3.7 && sudo apt --assume-yes install python3-pip && rm -r /opt/ha-bt-extender && mkdir -p /opt/ha-bt-extender && git clone -q https://github.com/sharafullin/ha-bt-extender.git /opt/ha-bt-extender && pip3 install -r /opt/ha-bt-extender/requirements.txt 
+sudo apt-get update && sudo apt-get --assume-yes install python3.7 && sudo apt --assume-yes install python3-pip && rm -r /opt/ha-bt-extender && mkdir -p /opt/ha-bt-extender && git clone -q https://github.com/sharafullin/ha-bt-extender.git /opt/ha-bt-extender 
+#&& pip3 install -r /opt/ha-bt-extender/requirements.txt 
 #&& sh /opt/ha-bt-extender/create_core_service.sh
 
 #pip3 install git+git://github.com/sharafullin/ha-bt-extender.git#egg=ha-bt-extender &&
