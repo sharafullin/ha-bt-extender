1d382a138f8debf8e60be56a69c8ae9535b030b6
Andrei Sharafullin <andrei.sharafullin@tomtom.com>
2020-09-19 22:38:18 +0200

    added tcp discovery

--- ha_bt_extender/internal_process.py
+++ ha_bt_extender/internal_process.py
@@ -1,9 +1,13 @@
 from multiprocessing import Process
 import udp_discovery
+import tcp_discovery
 
 def start():
     print("started of internal process")
     udp_discovery_process = Process(target=udp_discovery.start_udp_discovery)
     udp_discovery_process.start()
+    tcp_discovery_process = Process(target=tcp_discovery.start_tcp_discovery)
+    tcp_discovery_process.start()
 
     udp_discovery_process.join()
+    tcp_discovery_process.join()
