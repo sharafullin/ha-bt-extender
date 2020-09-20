5b9c05212d8d80d69044b1c92c3d1468d394a4b9
Andrei Sharafullin <andrei.sharafullin@tomtom.com>
2020-09-20 14:31:49 +0200

    added eq3 integration

--- ha_bt_extender/internal_process.py
+++ ha_bt_extender/internal_process.py
@@ -6,7 +6,7 @@ def start(logger: Queue):
     logger.put("started of internal process")
     udp_discovery_process = Process(target=udp_discovery.start_udp_discovery, args=(logger,))
     udp_discovery_process.start()
-    tcp_discovery_process = Process(target=tc   p_discovery.start_tcp_discovery, args=(logger,))
+    tcp_discovery_process = Process(target=tcp_discovery.start_tcp_discovery, args=(logger,))
     tcp_discovery_process.start()
 
     udp_discovery_process.join()
