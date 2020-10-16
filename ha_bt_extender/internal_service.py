e3768b72f9094660c5be3a1d0177acddfce7477e
Andrei Sharafullin <andrei.sharafullin@tomtom.com>
2020-10-16 21:20:53 +0200

    implemented logging

--- ha_bt_extender/internal_service.py
+++ ha_bt_extender/internal_service.py
@@ -1,21 +1,24 @@
-import time, sched
+import time, sched, logging
 from multiprocessing import Process, Queue
 import udp_discovery
 import tcp_discovery
 
+logging.basicConfig(level="INFO")
+
 logger = Queue()
 q = Queue()
+logging.info("Service started")
 
 def heartbeat():
     print(time.time(), "heartbeat")
     while not logger.empty():
         data = logger.get(timeout=0.5)
-        print("log: ", data)
+        logging.info(data)
+
 
 s = sched.scheduler(time.time, time.sleep)
 s.enter(30, 1, heartbeat)
 
-logger.put("started of internal service")
 udp_discovery_process = Process(target=udp_discovery.start_udp_discovery, args=(logger,))
 udp_discovery_process.start()
 tcp_discovery_process = Process(target=tcp_discovery.start_tcp_discovery, args=(logger, q))
